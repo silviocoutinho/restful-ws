99baa76e6303c3766afc8912e7ef8b55c4ba81c7
Silvio Coutinho <silviofc79@gmail.com>
2019-07-22 23:26:48 -0300

    first commit

---
+++ src/services/mysql/index.js
@@ -0,0 +1,23 @@
+const mysqlServer = require('mysql')
+
+const connection = mysqlServer.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+})
+
+const errorHandler = (error, msg, rejectFunction) =>{
+    console.log(error)
+    rejectFunction({error: msg})
+    
+}
+
+const categoryModule = require('./categories')({connection, errorHandler})
+
+
+module.exports = {
+    categories: () =>  {
+        return categoryModule
+    }
+} 
